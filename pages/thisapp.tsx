aae190c769b893811bf8222732ad1f9c6779fa44
Boris Baumann <boris.baumann@gmail.com>
2021-03-23 00:14:21 +0100

    Update pages/thisapp.tsx
    Co-authored-by: Ronja Meinecke <77615251+RonjaMeinecke@users.noreply.github.com>

--- pages/thisapp.tsx
+++ pages/thisapp.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
-      thisapp view
+      thisApp view
       <BottomNavigation />
     </div>
   );
