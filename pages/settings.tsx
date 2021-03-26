483d1f4ef221e278a5b505b49ee45133587764d5
HorstFrank <boris.baumann@gmail.com>
2021-03-26 10:04:57 +0100

    update settings (rr hotfix)

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import BottomNavigation from "../components/bottomnav/BottomNavigation";
 import Header from "../components/header/Header";
 import styles from "../styles/Home.module.css";
+import ReactPlayer from "react-player/youtube";
 
 export default function Home() {
-  return <div className={styles.container}>settings view</div>;
+  return (
+    <div className={styles.container}>
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        playing={true}
+      />
+    </div>
+  );
 }
